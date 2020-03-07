c5596a0078f697301fea7f84a54b32a66d7d5216
Daniel Angulo <dangulo4@gmail.com>
2020-03-07 13:23:11 -0800

    copy over

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 // Dependencies
-var http = require("http");  
 var fs = require("fs");
-
+var express = require("express");
+var path = require("path");
+// Setting our express app
+var app = express();
 // Set our port to 8000
 var PORT = 8000;
-
-// Create our server
-var server = http.createServer(handleRequest);
-
-// Create a function for handling the requests and responses coming into our server
-function handleRequest(req, res) {
-
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
   // Here we use the fs package to read our index.html file
   fs.readFile(__dirname + "/index.html", function(err, data) {
     if (err) throw err;
@@ -19,17 +17,7 @@ function handleRequest(req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(data);
   });
-}
-
 // Starts our server
-server.listen(PORT, function() {
-  console.log("Server is listening on PORT: " + PORT);
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
-
-
-// Create variables to hold data
-var tables = [
-  {
-    routeName: 
-  }
-] 
